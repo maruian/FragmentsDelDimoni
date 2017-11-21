eda1cf958dd1cdf4c4ad4020316c5d51647ff243
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-11-21 10:31:21 +0100

    Arreglar Framgnet estatico

--- app/src/main/java/com/example/admin/fragmentsdeldimoni/MainActivity.java
+++ app/src/main/java/com/example/admin/fragmentsdeldimoni/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.admin.fragmentsdeldimoni;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.admin.fragmentsdeldimoni.FragmentPrincipal;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onFragmentInteraction(Uri uri){}
+
 }
